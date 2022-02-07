5892f9f9028c8fc50fd98b7d9a2f25585f91b7af
Javier Escacena Sánchez <fjescacena@paradigmadigital.com>
2022-02-07 19:23:04 +0100

    random api for quotes & marvel oldies

---
+++ jesidea_api_functions/generic-quotes-random/generic-quotes-random.ts
@@ -0,0 +1,17 @@
+import { Handler } from '@netlify/functions';
+const axios = require('axios').default;
+
+export const handler: Handler = async (event, context) => {
+
+    const { data, errors } = await axios.get(`https://cercemap.org/resources/quotes/general-quotes.es.json`, {});
+
+    const items = data.data;
+
+    // 2.- Get random image
+    var randomItem = items[Math.floor(Math.random() * items.length)];
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(randomItem),
+    }
+}
---
+++ jesidea_api_functions/moapp-random/moapp-random.ts
@@ -0,0 +1,30 @@
+import { Handler } from '@netlify/functions'
+const axios = require('axios').default;
+
+
+export const handler: Handler = async (event, context) => {
+
+    const { age = 'goldenAge' } = event.queryStringParameters
+
+    // 1.- Get json data
+    const jsonFile = `${age}.characters.json`;
+    const { data, errors } = await axios.get(`https://cercemap.org/resources/moapp/json/${jsonFile}`, {});
+
+    // Error handling
+    if (errors) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(errors)
+        };
+    }
+
+    const items = data;
+
+    // 2.- Get random image
+    var randomItem = items[Math.floor(Math.random() * items.length)];
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(randomItem),
+    }
+}
