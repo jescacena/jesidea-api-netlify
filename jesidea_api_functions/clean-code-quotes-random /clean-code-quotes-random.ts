caf8c8fd04554ef1d34016c5f2d4549fe126ecf9
Javier Escacena Sánchez <fjescacena@paradigmadigital.com>
2022-02-21 11:39:12 +0100

    clean code quotes API

---
+++ jesidea_api_functions/clean-code-quotes-random /clean-code-quotes-random.ts
@@ -0,0 +1,16 @@
+import { Handler } from '@netlify/functions';
+const axios = require('axios').default;
+
+export const handler: Handler = async (event, context) => {
+
+    const { data, errors } = await axios.get(`https://cercemap.org/resources/quotes/clean-code-quotes.json`, {});
+
+    const items = data.data;
+
+    var randomItem = items[Math.floor(Math.random() * items.length)];
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(randomItem),
+    }
+}
